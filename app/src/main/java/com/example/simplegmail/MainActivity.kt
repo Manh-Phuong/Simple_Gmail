5d4b3be886c8cb7ebef86e378bffe1d7cdd25a10
Manh-Phuong <phuong.nm23112002@gmail.com>
2023-10-28 23:59:54 +0700

    first commit

---
+++ app/src/main/java/com/example/simplegmail/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.simplegmail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val messages = arrayListOf<MessageModel>()
+        messages.add(MessageModel("Mạnh Phương", "Bài tập clone giao diện gmail", "23:11 PM", false))
+        messages.add(MessageModel("Edurila.com", "$19 Only (First 10 spots) - Bestselling", "12:34 PM", false))
+        messages.add(MessageModel("Chris Abad", "Help make Campaign Monitor better Let us know your thoughts! NO images", "11:22 AM", false))
+        messages.add(MessageModel("Tuto.com", "8h de formation gratuite et les nouvea Photoshop, SEO, Blender, CSS, WordPress", "11:04 AM", false))
+        messages.add(MessageModel("support", "Announcing the all-new", "10:26 AM", false))
+        messages.add(MessageModel("Matt from lonic", "The New lonic Creator is here", "9:23 AM", false))
+
+        findViewById<ListView>(R.id.list_view).adapter = MessageAdapter(messages)
+    }
+}
